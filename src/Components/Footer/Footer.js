e5434f727bbdb01b017574c4582cd754df198436
huynguyen321 <huy.nguyen22@student.passerellesnumeriques.org>
2021-05-17 19:21:28 +0700

    add send email

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,30 @@
 import React from "react";
-import './Footer.css'
-import Images from 'Constants/images'
+import "./Footer.css";
+import Images from "Constants/images";
+import sendEmail from "../../SendEmail/SendEmail";
+
+import $ from "jquery";
 class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmitReceiveInfor = this.handleSubmitReceiveInfor.bind(this);
+  }
+
+  handleSubmitReceiveInfor(e) {
+    e.preventDefault();
+    if ($("#emailReceiveNewInfor").val() != "") {
+      let emailReceiveNewInfor = $("#emailReceiveNewInfor").val();
+      let contentEmail =
+        "Cảm ơn quý khách đã đăng ký để nhận thông tin mới nhất từ shop.";
+      sendEmail(emailReceiveNewInfor, contentEmail)
+        .then((res) => {
+          alert("Gửi email thành công");
+        })
+        // Handle errors here however you like, or use a React error boundary
+        .catch((err) => console.error("Đã có lỗi xảy ra! Lỗi: " + err));
+    }else alert('Bạn chưa nhập địa chỉ email đăng ký nhận thông tin.')
+  }
+
   render() {
     return (
       <div className="footer">
@@ -15,9 +38,7 @@ class Footer extends React.Component {
                     alt="Totoro Milk Tea"
                     className="w-100"
                   />
-                  <p className="text-left">
-                    Totoro Milk Tea
-                  </p>
+                  <p className="text-left">Totoro Milk Tea</p>
                 </div>
               </div>
               <div className="col-md-3 col-sm-6">
@@ -52,21 +73,25 @@ class Footer extends React.Component {
                   <strong> Mới nhất</strong>
                 </h6>
                 <p>Nhập Email đăng ký nhận thông tin</p>
+
                 <form
-                  className="newsletter"
+                  className="newsletter form-inline"
+                  onSubmit={this.handleSubmitReceiveInfor}
                 >
-                  <input
-                    type="text"
-                    id="email"
-                    name="emailReceiveInfor"
-                    placeholder="Nhập Email...."
-                  />
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      id="emailReceiveNewInfor"
+                      className="form-control"
+                      placeholder="Nhập Email...."
+                    />
                     <button
                       type="submit"
                       className="btn btn-sm btn-outline-light"
                     >
                       Đăng ký
                     </button>
+                  </div>
                 </form>
               </div>
             </div>
@@ -78,31 +103,43 @@ class Footer extends React.Component {
               <div className="col-md-7">
                 <p>
                   ©2021 Copyright by
-                  <a href="https://www.facebook.com/ulrich.stern.319452/"> Nguyễn Ngọc Huy &amp; Nguyễn Thị Diễm <br/> &amp; Lê Thị Hồng Hạnh &amp; Phạm Anh Tuấn </a>
+                  <a href="https://www.facebook.com/ulrich.stern.319452/">
+                    {" "}
+                    Nguyễn Ngọc Huy &amp; Nguyễn Thị Diễm <br /> &amp; Lê Thị
+                    Hồng Hạnh &amp; Phạm Anh Tuấn{" "}
+                  </a>
                 </p>
               </div>
               <div className="col-md-5">
                 <ul className="social-icon">
                   <li>
-                    <a href="mailto:huy.nguyen22@student.passerellesnumeriques.org" className="google-plus">email</a>
+                    <a
+                      href="mailto:huy.nguyen22@student.passerellesnumeriques.org"
+                      className="google-plus"
+                    >
+                      email
+                    </a>
                   </li>
                   <li>
                     <a
                       href="https://www.linkedin.com/in/nguy%E1%BB%85n-ng%E1%BB%8Dc-huy-363b26207/"
                       className="linkedIn"
-                    >linkedIn</a>
+                    >
+                      linkedIn
+                    </a>
                   </li>
                   <li>
                     <a
                       href="https://www.facebook.com/ulrich.stern.319452/"
                       className="facebook"
-                    >FB</a>
+                    >
+                      FB
+                    </a>
                   </li>
                   <li>
-                    <a
-                      href="https://twitter.com/Nagamaru8"
-                      className="twitter"
-                    >Twitter</a>
+                    <a href="https://twitter.com/Nagamaru8" className="twitter">
+                      Twitter
+                    </a>
                   </li>
                 </ul>
               </div>
---
+++ src/SendEmail/SendEmail.js
@@ -0,0 +1,20 @@
+import { Email, Item, Span, renderEmail } from "react-html-email";
+
+const SendEmail = (to_email, message) => {
+  const message_html = renderEmail(
+    <Email title="Thư gửi từ Totoro Milk Tea Shop">
+      <Item align="center">
+        <Span fontSize={20}>{message}</Span>
+      </Item>
+    </Email>
+  );
+  const variables = {
+    message_html: message,
+    to_email: to_email,
+  };
+  console.log(variables);
+  //params: email_serviceId, templateId,variables(have in template Email)
+  return window.emailjs.send("shop_email", "totoro_email", variables);
+};
+
+export default SendEmail;
