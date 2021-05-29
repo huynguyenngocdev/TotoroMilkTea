7aed30eea1da3b17b338df0474568e3ab30193e7
huynguyen321 <huy.nguyen22@student.passerellesnumeriques.org>
2021-05-30 01:16:38 +0700

    fix ads when browser ban ads

--- src/Components/Advertisement/Ads.js
+++ src/Components/Advertisement/Ads.js
@@ -27,12 +27,17 @@ class Ads extends Component {
         }));
       }
     });
-
-    callAPI(`get_image/${this.state.adsInfor.image}`, "GET", null).then(
-      (res) => {
-        this.setState(() => ({ imageAds: res.data.image }));
-      }
-    );
+    try {
+      callAPI(`get_image/${this.state.adsInfor.image}`, "GET", null).then(
+        (res) => {
+          this.setState(() => ({ imageAds: res.data.image }));
+        }
+      );
+    } catch {
+      alert(
+        "Không thể hiển thị quảng cáo vui lòng tắt chặn quảng cáo trên trình duyêt để website được hoạt động tốt nhất."
+      );
+    }
 
     this.timerID = setInterval(() => this.tick(), 1000);
   }
